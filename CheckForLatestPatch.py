fac2b2eb2316d37cc67a0126a7534107f3df3813
ad\tobias.helling <Tobias.helling@connect.se>
2023-04-20 21:21:53 +0200

    Test

--- CheckForLatestPatch.py
+++ CheckForLatestPatch.py
@@ -1,4 +1,5 @@
 # Kikar om servern är fullt patchad
+
 $criteria = "Type='software' and IsAssigned=1 and IsHidden=0 and IsInstalled=0"
 
 $searcher = (New-Object -COM Microsoft.Update.Session).CreateUpdateSearcher()
