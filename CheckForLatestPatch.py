40b7ab262b317be0f609fb1ddec4c81d734e0b0d
ad\tobias.helling <Tobias.helling@connect.se>
2023-04-20 21:16:01 +0200

    Test

---
+++ CheckForLatestPatch.py
@@ -0,0 +1,13 @@
+# Kikar om servern är fullt patchad
+$criteria = "Type='software' and IsAssigned=1 and IsHidden=0 and IsInstalled=0"
+
+$searcher = (New-Object -COM Microsoft.Update.Session).CreateUpdateSearcher()
+$updates  = $searcher.Search($criteria).Updates
+
+if ($updates.Count -ne 0) {
+  # $updates pending
+  write-host "Mindre bra"
+} else {
+  # system up-to-date
+  write-host "Bra"
+}
